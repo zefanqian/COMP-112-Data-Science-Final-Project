1003530d6f965d2631a9f12c075352a8332afbf4
zefanqian <70648948+zefanqian@users.noreply.github.com>
2021-07-02 03:16:06 +0800

    Update Conditional Panel

--- App_file.R
+++ App_file.R
@@ -9,60 +9,88 @@ data_cleaned<- data %>%
   select(title, category_id, tags, views, likes, comment_count, dislikes)
 
 ui <- fluidPage(
-  textInput("tags", 
-            "Tags", 
-            value = "", 
-            placeholder = ""),
-  selectInput("category",
-              "Category",
-              choices = list("Autos & Vehicles" = "1", "Music" = "2", "Comedy" = "10", "Science & Technology" = "15", "Movies" = "17", "Action/Adventure" = "19", 
-                             "Documentary" = "22", "Drama" = "23", "Family" = "24", "Horror" = "26", "Sci-Fi/Fantasy" = "27", "Thriller" = "28")),
-  mainPanel(
-    tabsetPanel(
-      id = 'dataset',
-      tabPanel("Likes", DT::dataTableOutput("mytable_likes")),
-      tabPanel("Dislikes", DT::dataTableOutput("mytable_dislikes")),
-      tabPanel("Comment Count", DT::dataTableOutput("mytable_comment_count")),
-      tabPanel("Views", DT::dataTableOutput("mytable_views"))
+  title = "Youtube Recommendation",
+  sidebarLayout(
+    sidebarPanel(
+      conditionalPanel(
+        'input.dataset === "Likes"',
+        selectInput("category1",
+                    "Category",
+                    choices = list("Autos & Vehicles" = "1", "Music" = "2", "Comedy" = "10", "Science & Technology" = "15", "Movies" = "17", "Action/Adventure" = "19", 
+                                   "Documentary" = "22", "Drama" = "23", "Family" = "24", "Horror" = "26", "Sci-Fi/Fantasy" = "27", "Thriller" = "28")),
+      ),
+      conditionalPanel(
+        'input.dataset === "Dislikes"',
+        selectInput("category2",
+                    "Category",
+                    choices = list("Autos & Vehicles" = "1", "Music" = "2", "Comedy" = "10", "Science & Technology" = "15", "Movies" = "17", "Action/Adventure" = "19", 
+                                   "Documentary" = "22", "Drama" = "23", "Family" = "24", "Horror" = "26", "Sci-Fi/Fantasy" = "27", "Thriller" = "28")),
+      ),
+      conditionalPanel(
+        'input.dataset === "Comment Count"',
+        selectInput("category3",
+                    "Category",
+                    choices = list("Autos & Vehicles" = "1", "Music" = "2", "Comedy" = "10", "Science & Technology" = "15", "Movies" = "17", "Action/Adventure" = "19", 
+                                   "Documentary" = "22", "Drama" = "23", "Family" = "24", "Horror" = "26", "Sci-Fi/Fantasy" = "27", "Thriller" = "28")),
+      ),
+      conditionalPanel(
+        'input.dataset === "Views"',
+        selectInput("category4",
+                    "Category",
+                    choices = list("Autos & Vehicles" = "1", "Music" = "2", "Comedy" = "10", "Science & Technology" = "15", "Movies" = "17", "Action/Adventure" = "19", 
+                                   "Documentary" = "22", "Drama" = "23", "Family" = "24", "Horror" = "26", "Sci-Fi/Fantasy" = "27", "Thriller" = "28")),
+      )
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = 'dataset',
+        tabPanel("Likes", DT::dataTableOutput("mytable_likes")),
+        tabPanel("Dislikes", DT::dataTableOutput("mytable_dislikes")),
+        tabPanel("Comment Count", DT::dataTableOutput("mytable_comment_count")),
+        tabPanel("Views", DT::dataTableOutput("mytable_views"))
+      )
     )
   )
 )
 
+
 server <- function(input, output) {
-  
-  output$mytable_likes <- DT::renderDataTable({
-    data_cleaned %>%
-      mutate(likes = as.numeric(likes)) %>%
-      filter(category_id == input$category) %>%
-      arrange(desc(likes)) %>%
-      select(title, likes)
-  })
-  
-  output$mytable_dislikes <- DT::renderDataTable({
-    data_cleaned %>%
-      mutate(dislikes = as.numeric(dislikes)) %>%
-      filter(category_id == input$category) %>%
-      arrange(dislikes) %>%
-      select(title, dislikes)
-  })
-  
-  output$mytable_comment_count <- DT::renderDataTable({
-    data_cleaned %>%
-      mutate(comment_count = as.numeric(comment_count)) %>%
-      filter(category_id == input$category) %>%
-      arrange(desc(comment_count)) %>%
-      select(title, comment_count)
-  })
-  
-  output$mytable_views <- DT::renderDataTable({
-    data_cleaned %>%
-      mutate(views = as.numeric(views)) %>%
-      filter(category_id == input$category) %>%
-      arrange(desc(views)) %>%
-      select(title, views)
-  })
-  
-}
+    
+    output$mytable_likes <- DT::renderDataTable({
+      data_cleaned %>%
+        mutate(likes = as.numeric(likes)) %>%
+        filter(category_id == input$category1) %>%
+        arrange(desc(likes)) %>%
+        select(title, likes)
+    })
+    
+    output$mytable_dislikes <- DT::renderDataTable({
+      data_cleaned %>%
+        mutate(dislikes = as.numeric(dislikes)) %>%
+        filter(category_id == input$category2) %>%
+        arrange(dislikes) %>%
+        select(title, dislikes)
+    })
+    
+    output$mytable_comment_count <- DT::renderDataTable({
+      data_cleaned %>%
+        mutate(comment_count = as.numeric(comment_count)) %>%
+        filter(category_id == input$category3) %>%
+        arrange(desc(comment_count)) %>%
+        select(title, comment_count)
+    })
+    
+    output$mytable_views <- DT::renderDataTable({
+      data_cleaned %>%
+        mutate(views = as.numeric(views)) %>%
+        filter(category_id == input$category4) %>%
+        arrange(desc(views)) %>%
+        select(title, views)
+    })
+    
+  }
 
 shinyApp(ui = ui, server = server)
+
+
 
